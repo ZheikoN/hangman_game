585ecfa0af6733341eb947103946b46b7fa7ac9a
ZheikoN <negru.inger@gmail.com>
2022-07-28 11:24:58 +0000

    redone the logic of the if statements and squashed some bugs

--- run.py
+++ run.py
@@ -11,44 +11,48 @@ def select_word():
     Randomly selected word from list of words
     """
     selected_word = random.choice(words)
-    return selected_word
+    return selected_word.upper()
 
 
 def game_logic(word):
-    word_list = list(word)
-    wrong_letter = set()
-    correct_letter = set()
-    
-    hashed_word = ['-' for letter in word_list]
-    print("Guess the word", "".join(hashed_word))
-
-    while len(word_list) > 0 and len(wrong_letter) < 5:
-        chosen_letter = input("enter a letter \n")
-        semihashed_word = [letter if letter in correct_letter else '-' for letter in word_list]
-        print(''.join(semihashed_word))
-        if chosen_letter in banned_characters:
-            print("you can use only letters, numbers are not used")
-        elif chosen_letter in wrong_letter.union(correct_letter):
-            print("this letter was already guessed")
-        elif len(chosen_letter) > 1:
-            print(f"You can guess only single letter, you guessed {len(chosen_letter)}")
-        elif len(chosen_letter) == 1:
-            if chosen_letter in word:
-                print("yay")
-                correct_letter.add(chosen_letter)
-                print('correct letters so far:', '-'.join(correct_letter))
-                
+    """
+    This is the main logic of the game
+    """
+    print("Hangman")
+    hashed_word = "_" * len(word)
+    print("Guessing this word: " + hashed_word)
+    used_letter = []
+    success_letter = []
+    lives = 5
+    while lives > 0:
+        guessed_letter = input("guess your letter\n").upper()
+        if guessed_letter in word:
+            if guessed_letter in used_letter:
+                print("this letter was already used")
+            elif guessed_letter not in word:
+                print(f"wrong guess, amount of lives left:{lives - 1}")
+                lives -= 1
+                used_letter.append(guessed_letter)
+            elif guessed_letter in banned_characters:
+                print("no numbers please")
             else:
-                print("nay")
-                wrong_letter.add(chosen_letter)
-                guessed_letter = wrong_letter.union(correct_letter)
-                print(f"So far you have tried these letters: {guessed_letter}")            
-        
-    # if len(wrong_letter) == 5:
-    #     print("you lost")
-    # elif len(word_list) == 0:
-    #     print("you won!")
-        
+                print("success!")
+                used_letter.append(guessed_letter)
+                success_letter.append(guessed_letter)
+                semihashed_word = [letter if letter in success_letter else '-' for letter in word]
+                print(semihashed_word)
+                if '-' not in semihashed_word:
+                    print("you win")
+                    break
+        elif guessed_letter not in word:
+            if guessed_letter in used_letter:
+                print("this letter was already used")
+            else:
+                print(f"wrong guess, amount of lives left:{lives - 1}")
+                lives -= 1
+                used_letter.append(guessed_letter)
+        else:
+            print("something is wrong. Exiting \n")
 
 word = select_word()
 game_logic(word)
