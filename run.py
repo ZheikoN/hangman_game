f648677ce709b17bc8f38fd65116d5c62de044f7
ZheikoN <negru.inger@gmail.com>
2022-08-02 13:18:50 +0000

    fix indentation and line break for clear and easy to read code as per PEP8 checker

--- run.py
+++ run.py
@@ -25,21 +25,25 @@ def game_logic(word):
     lives = 5
     while lives > 0:
         guessed_letter = input("Guess your letter: \n").upper()
-        if guessed_letter in word and len(guessed_letter) == 1 and guessed_letter.isalpha() is True:
+        if guessed_letter in word and len(guessed_letter) == 1 and \
+           guessed_letter.isalpha() is True:
             if guessed_letter in used_letter:
-                print("This letter was already used. \nUsed letters: " + "-".join(used_letter))
+                print("This letter was already used. \nUsed letters: " +
+                      "-".join(used_letter))
             else:
                 print("Success!")
                 used_letter.append(guessed_letter)
                 success_letter.append(guessed_letter)
-                semihashed_word = [letter if letter in success_letter else '_' for letter in word]
+                semihashed_word = [letter if letter in success_letter else '_'
+                                   for letter in word]
                 print(' '.join(semihashed_word))
                 if '_' not in semihashed_word:
                     print(win)
                     break
         elif guessed_letter not in word:
             if guessed_letter in used_letter:
-                print("\nThis letter was already used. \nUsed letters: " + "-".join(used_letter))
+                print("\nThis letter was already used. \nUsed letters: " +
+                      "-".join(used_letter))
             elif len(guessed_letter) != 1:
                 print("Only single letters please!")
             elif guessed_letter.isalpha() is False:
@@ -47,25 +51,26 @@ def game_logic(word):
             else:
                 lives -= 1
                 used_letter.append(guessed_letter)
+                msg = "You have tried these letters: " + "-".join(used_letter)
                 if lives == 4:
                     print(four)
-                    print("You have tried these letters: " + "-".join(used_letter))
+                    print(msg)
                 elif lives == 3:
                     print(three)
-                    print("You have tried these letters: " + "-".join(used_letter))
+                    print(msg)
                 elif lives == 2:
                     print(two)
-                    print("You have tried these letters: " + "-".join(used_letter))
+                    print(msg)
                 elif lives == 1:
                     print(one)
-                    print("You have tried these letters: " + "-".join(used_letter))
+                    print(msg)
                 else:
                     print(over)
                     print(f"Sorry that you hang, the word was '{word}'")
                     break
-            print(f"\nWrong guess, amount of lives left: {lives}")    
+            print(f"\nWrong guess, amount of lives left: {lives}")
         else:
-            print("\nSomething is wrong(did you enter anything?). Try again \n")
+            print("\nMust be a letter. Try again \n")
 
 
 word = select_word()
